c23328462deca2adab2a79c7f73d7dda78da0470
Sarah <92927637+SarahGrozav1@users.noreply.github.com>
2023-11-25 13:56:43 +0000

    Connecting my app to the remote database

--- codestar/settings.py
+++ codestar/settings.py
@@ -1,3 +1,8 @@
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
+
 """
 Django settings for codestar project.
 
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t7feo$3=v478gf2s4b_*75yuk&)7y3)uz+ca7z%4%x_)thq6+s'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -74,11 +79,15 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+DATABASE = {
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
